fce9b72cd2646ca82991751e68d2ef9430457788
Karl97490 <karl.pery@icloud.com>
2026-05-28 14:21:48 +0400

    feat: addind routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import movies from "./data/movies.json";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
+
 function App() {
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
@@ -16,7 +20,20 @@ function App() {
   return (
     <>
       <Header numberOfMovies={moviesToDisplay.length} />
-      <MovieList moviesArr={moviesToDisplay} deleteFunction={deleteMovie} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MovieList
+              moviesArr={moviesToDisplay}
+              deleteFunction={deleteMovie}
+            />
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<div>401</div>}></Route>
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Header = ({ numberOfMovies }) => {
   return (
-    <header className="Header">
-      <h1>Welcome to PopCorn-Time!</h1>
-      <h2>Numbers of movie: {numberOfMovies}</h2>
-    </header>
+    <div className="Header">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+      <header>
+        <h1>Welcome to PopCorn-Time!</h1>
+        <h2>Numbers of movie: {numberOfMovies}</h2>
+      </header>
+    </div>
   );
 };
 
